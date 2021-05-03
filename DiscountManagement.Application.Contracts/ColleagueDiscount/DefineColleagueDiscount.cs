4b9da253213ed597aadc48dffd9d5c8c6d5409a9
rostamani <rezarostamani.sru@gmail.com>
2021-05-04 00:32:43 +0430

    Apply Validations In Discount Panel

--- DiscountManagement.Application.Contracts/ColleagueDiscount/DefineColleagueDiscount.cs
+++ DiscountManagement.Application.Contracts/ColleagueDiscount/DefineColleagueDiscount.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.ProductAgg;
 
 namespace DiscountManagement.Application.Contracts.ColleagueDiscount
 {
     public class DefineColleagueDiscount
     {
+        [Range(1, 1000000, ErrorMessage = ValidationMessage.IsRequired)]
         public long ProductId { get; set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public int DiscountRate { get; set; }
         public List<SelectProductViewModel> Products { get; set; }
     }
--- DiscountManagement.Application.Contracts/CustomerDiscount/DefineCustomerDiscount.cs
+++ DiscountManagement.Application.Contracts/CustomerDiscount/DefineCustomerDiscount.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.ProductAgg;
 
 namespace DiscountManagement.Application.Contracts.CustomerDiscount
 {
     public class DefineCustomerDiscount
     {
-        public int ProductId { get;  set; }
+        [Range(1,1000000,ErrorMessage = ValidationMessage.IsRequired)]
+        public long ProductId { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public int DiscountRate { get;  set; }
         public string Reason { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string StartDate { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string EndDate { get;  set; }
         public List<SelectProductViewModel> Products { get; set; }
     }
